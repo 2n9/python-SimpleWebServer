39a1acd0d456260b1b4891b658983a2f1b02d3d6
yyt <92621736+2n9@users.noreply.github.com>
2021-12-28 16:59:33 +0900

    commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# python simple webserver
+import http.server
+import socketserver
+import os
+
+def create_server(port):
+    os.chdir('./public')
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", int(port)), Handler)
+    print("serving at port", port)
+    httpd.serve_forever()
+
+def restart_server(port):
+    httpd.shutdown()
+    create_server(port)
+
+async def get_command():
+    command = input("$ ")
+    return command
+
+def handle_command(command):
+    if command == "stop":
+        httpd.shutdown()
+        print("server closed")
+    elif command == "start":
+        httpd.shutdown()
+        port = input("port: ")
+        create_server(port)
+    elif command == "help":
+        print("""
+        stop - close server
+        restart - restart server
+        help - show this message
+        """)
+    else:
+        print("command not found")
+
+def main():
+    while True:
+        command = get_command()
+        handle_command(command)
+
+
+if __name__ == "__main__":
+    main()
